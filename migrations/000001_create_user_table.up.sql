4fe34c7825cabaf454f0df1fe2bdd0a1345ee5cc
Nikita Koida <nikita.koido@intexsoft.by>
2022-03-31 13:56:25 +0300

    Update users migration table

--- migrations/000001_create_user_table.up.sql
+++ migrations/000001_create_user_table.up.sql
@@ -1,8 +1,11 @@
-CREATE TABLE users(
-                      id BIGINT PRIMARY KEY,
-                      email VARCHAR(100),
-                      password  VARCHAR(255),
-                      firstName VARCHAR(50),
-                      lastName VARCHAR(50),
-                      active tinyint UNSIGNED
+CREATE TABLE users
+(
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email     VARCHAR(100) NOT NULL,
+    password  VARCHAR(255),
+    firstName VARCHAR(50),
+    lastName  VARCHAR(50),
+    active    tinyint UNSIGNED,
+    CONSTRAINT users_email_uindex
+        UNIQUE (email)
 )
