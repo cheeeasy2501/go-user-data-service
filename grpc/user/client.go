a7fe67262c5384a2cc8bbd04b5621a837f51e966
Nikita Koida <nikita.koido@intexsoft.by>
2022-04-01 15:16:55 +0300

    Complete test auth with go-user-data-service by GRPC

--- grpc/user/client.go
+++ grpc/user/client.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"context"
+	m "github.com/cheeeasy2501/go-user-data-service/internal/model"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
@@ -16,8 +18,8 @@ func NewClient() *GRPCClient {
 }
 
 func (c *GRPCClient) Open() {
+	//TODO CHECK LIB OR METHOD LAZYLOAD FOR GRPC CONN.
 	conn, err := grpc.Dial(":8081", grpc.WithTransportCredentials(insecure.NewCredentials()))
-
 	if err != nil {
 		log.Println("Connection not open!", err)
 	}
@@ -31,4 +33,24 @@ func (c *GRPCClient) Close() {
 	if err != nil {
 		log.Println("Connection not close!", err)
 	}
+}
+
+func (c *GRPCClient) GetUserData(ctx context.Context, id uint64) (*m.User, error) {
+	req := &GetUserRequest{
+		Id: id,
+	}
+
+	data, err := c.Client.GetUserData(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &m.User{
+		Id:        data.GetId(),
+		Email:     data.GetEmail(),
+		Password:  data.GetPassword(),
+		FirstName: data.GetFirstName(),
+		LastName:  data.GetLastName(),
+		Active:    data.GetActive(),
+	}, nil
 }
